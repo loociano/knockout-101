a22d13232e1fe34aa38cead73228eca5f91c5b14
Luciano Rubio <luciano@loociano.com>
2015-09-07 22:38:40 +0100

    Fully tested click-counter

--- click-counter/test/tests.js
+++ click-counter/test/tests.js
@@ -1,7 +1,46 @@
 describe('DOM tests', function() {
 
-  it('should be a button', function(){
+  it('should display a click button', function(){
     expect(document.getElementById('button')).to.not.equal(undefined);
+  });
+
+  it('should not display a warning message', function(){
+    expect(document.getElementById('warning').style.display).to.equal('none');
+  });
+});
+
+describe('View Model tests', function(){
+
+  afterEach(function(){
+    viewModel.resetClicks();
+  });
+
+  it('click counter should start at zero', function(){
+    expect(viewModel.numberOfClicks()).to.equal(0);
+  });
+
+  it('2 clicks should increase twice the counter viewModel', function(){
+    viewModel.registerClick();
+    viewModel.registerClick();
+    expect(viewModel.numberOfClicks()).to.equal(2);
+  });
+
+  it('3 clicks should disable the button and display a warning', function(){
+    viewModel.registerClick();
+    viewModel.registerClick();
+    viewModel.registerClick();
+    expect(viewModel.numberOfClicks()).to.equal(3);
+    expect(document.getElementById('button').disabled).to.equal(true);
+    expect(document.getElementById('warning').style.display).to.not.equal('none');
+  });
+
+  it('button reset should reset the counter viewModel', function(){
+    viewModel.registerClick();
+    viewModel.registerClick();
+    viewModel.registerClick();
+    viewModel.resetClicks();
+    expect(document.getElementById('button').disabled).to.equal(false);
+    expect(document.getElementById('warning').style.display).to.equal('none');
   });
 
 });
